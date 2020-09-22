c9bfa430e5a98fe0f45afaaf3a7258994247d4ff
James <jamesmccafferty@live.ca>
2020-09-22 14:59:16 -0400

    Add boilerplate for testing and local server, remove config idea

--- code/devServer.py
+++ code/devServer.py
@@ -1,10 +1,26 @@
-# simple script that runs the api as a local server
-# console output will provide endpoint ip. Normall localhost:5000
+# run from command line, starts a development server used to test the api
+# run as `python devServer.py`, automatically attempts to create server at localhost:5000
+#
+# if for some reason then ip is in use, the following command can be run
+# `python devServer.py ip port` will use provided ip and port. 
+# Will fail if ip or port are invalid 
 
 import api.dadaApi as api
+import sys
 
-def run():
-  api.api.run()
+def initializeDevServer():
+  # Determine the config path
+  args = sys.argv
+  lenargs = len(args)
+
+  # load the config json and detemrine host
+  defaultServerIp = "localhost" 
+  defaultServerPort = "5000" 
+
+  serverIp = defaultServerIp if lenargs < 3 else args[1]
+  serverPort = defaultServerPort if lenargs < 3 else args[2]
+
+  api.api.run(serverIp,serverPort)
 
 if(__name__ == "__main__"):
-    run()
+    initializeDevServer()
--- code/unitTest.py
+++ code/unitTest.py
@@ -1,5 +1,28 @@
-# Run dev server and performs api unit tests
+# run from command line, starts a development server used to test the api
+# run as `python unitTest.py`, automatically attempts to create server at localhost:5000
+#
+# if for some reason then ip is in use, the following command can be run
+# `python unitTest.py ip port` will use provided ip and port. 
+# Will fail if ip or port are invalid 
 
 import api.dadaApi as api
+import sys
+import requests
 
+def runTests():
+  args = sys.argv
+  lenargs = len(args)
 
+  # load the config json and detemrine host
+  defaultServerIp = "localhost" 
+  defaultServerPort = "5000" 
+
+  serverIp = defaultServerIp if lenargs < 3 else args[1]
+  serverPort = defaultServerPort if lenargs < 3 else args[2]
+
+  url = f"http://{serverIp}:{serverPort}/companies"
+  res = requests.get(url = url)
+  print(res.json())
+
+if(__name__ == "__main__"):
+  runTests()
